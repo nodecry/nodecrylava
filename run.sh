4e857cd8cf71b469e82bdb19746c19f2ece986bb
nodecry <nodecry738@gmail.com>
2024-03-22 01:29:50 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-e5fb0881-3b03-485b-b573-77791571ce12/)
 
 
 echo "$curl_output" >> Update.tsx
